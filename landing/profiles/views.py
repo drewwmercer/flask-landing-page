28e6e61075b5306dfe818227c4fe0eaeaa7ced8e
drewwmercer <awmercer@gmail.com>
2021-02-22 17:33:06 -0500

    sending a dictionary as a varaiable

--- landing/profiles/views.py
+++ landing/profiles/views.py
@@ -3,7 +3,8 @@ from landing import app
 
 @app.route('/user/<username>/')
 def profile(username):
-    return render_template('profiles_detail.html', username=username)
+    context = {'user': username}
+    return render_template('profiles_detail.html', context=context, username=username, some_list=[1,22,333,4444,55555])
 
 @app.route('/users/')
 def profiles_list():
