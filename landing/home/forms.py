59d3a3acb609a4d60dd075deb039a0c46fecd21f
drewwmercer <awmercer@gmail.com>
2021-02-22 20:08:33 -0500

    validating required fields

--- landing/home/forms.py
+++ landing/home/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField
+from wtforms import StringField, validators
 
 class LandingForm(FlaskForm):
-    full_name = StringField('Full Name')
-    email = StringField('Email')
+    full_name = StringField('Full Name', validators=[validators.DataRequired(message='Full name is required')])
+    email = StringField('Email', validators=[validators.DataRequired(message='Email is required')])
