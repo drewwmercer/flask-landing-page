0b42f98cfe4ca7698329cd4649976eb7826fad82
drewwmercer <awmercer@gmail.com>
2021-02-22 20:33:07 -0500

    cleaned form and error display

--- landing/home/forms.py
+++ landing/home/forms.py
@@ -2,8 +2,8 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, validators, ValidationError
 
 class LandingForm(FlaskForm):
-    full_name = StringField('Full Name', validators=[validators.DataRequired(message='Full name is required.')])
-    email = StringField('Email', validators=[validators.DataRequired(message='Email is required.'), validators.Email()])
+    full_name = StringField('Full Name', render_kw={'class': 'form-control', 'placeholder': 'Your name'}, validators=[validators.DataRequired(message='Full name is required.')])
+    email = StringField('Email', render_kw={'class': 'form-control', 'placeholder': 'Your email'}, validators=[validators.DataRequired(message='Email is required.'), validators.Email()])
 
     def validate_email(self, field):
         if field.data.endswith('.edu'):
