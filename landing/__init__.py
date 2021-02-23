81bbc68b780ee520cb47e3706ff74698b3ed66ea
drewwmercer <awmercer@gmail.com>
2021-02-23 17:59:51 -0500

    setting up crud convenience methods

--- landing/__init__.py
+++ landing/__init__.py
@@ -7,11 +7,11 @@ app = Flask(__name__)
 csrf = CSRFProtect(app)
 
 db_path = os.path.join(os.path.dirname(__file__), 'db.sqlite3')
-dburi = 'sqlite:///{}'.format(db_path) # tells Flask and SQLAlchemy where the db will be stored
+db_uri = 'sqlite:///{}'.format(db_path) # tells Flask and SQLAlchemy where the db will be stored
 
 # secret key
 # wtf csrf secret key
-app.config.update(dict(SECRET_KEY='2809a477-5087e-4043-b9d9-a971c0982e48d', WTF_CSRF_SECRET_KEY='4cb681ac-1afe1278f84d-c8e2-4e75-ba17-1afe1278f84d', SQLALCHEMY_DATABASE_URI=dburi))
+app.config.update(dict(SECRET_KEY='2809a477-5087e-4043-b9d9-a971c0982e48d', WTF_CSRF_SECRET_KEY='4cb681ac-1afe1278f84d-c8e2-4e75-ba17-1afe1278f84d', SQLALCHEMY_DATABASE_URI=db_uri, SQLALCHEMY_TRACK_MODIFICATIONS=True))
 
 db = SQLAlchemy(app)
 
--- landing/home/models.py
+++ landing/home/models.py
@@ -1,6 +1,32 @@
 from landing import db
 
 class EmailSignup(db.Model):
-    id          = db.Column(db.Integer)
+    id          = db.Column(db.Integer, primary_key=True)
     full_name   = db.Column(db.String(120), nullable=True)
     email       = db.Column(db.String(120), unique=True, nullable=False)
+
+    def save(self, commit=True):
+        if commit:
+            instance = self
+            if not instance.id:
+                db.session.add(instance)
+            try:
+                db.session.commit()
+            except Exception as e:
+                print('Exception occurred\n', e, '\n')
+                db.session.rollback()
+                return False     
+            return True
+        return False
+
+    def delete(self, commit=True):
+        if self.id:
+            db.session.delete(self)
+            try:
+                    db.session.commit()
+            except Exception as e:
+                print('Exception occurred\n', e, '\n')
+                db.session.rollback()
+                return False        
+            return True
+        return False
