9ee1cdd402f4c463c230ac5dbad4e11b04ad2ba6
drewwmercer <awmercer@gmail.com>
2021-02-22 20:01:30 -0500

    CSRF is working

--- landing/__init__.py
+++ landing/__init__.py
@@ -6,7 +6,7 @@ csrf = CSRFProtect(app)
 
 # secret key
 # wtf csrf secret key
-app.config.update(dict(SECRET_KEY='2809a477-5087e-4043-b9d9-a971c0982e48d',WTF_CSRF_SECRET_KEY='4cb681ac-1afe1278f84d-c8e2-4e75-ba17-1afe1278f84d'))
+app.config.update(dict(SECRET_KEY='2809a477-5087e-4043-b9d9-a971c0982e48d', WTF_CSRF_SECRET_KEY='4cb681ac-1afe1278f84d-c8e2-4e75-ba17-1afe1278f84d'))
 
 from .views import *
 from .home.views import *
