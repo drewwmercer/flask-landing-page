da6a16a781271b980e738445c51a43d665796d7c
drewwmercer <awmercer@gmail.com>
2021-02-23 18:09:57 -0500

    checking for duplicate emails

--- landing/home/forms.py
+++ landing/home/forms.py
@@ -1,6 +1,8 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, validators, ValidationError
 
+from .models import EmailSignup
+
 class LandingForm(FlaskForm):
     full_name = StringField('Full Name', render_kw={'class': 'form-control', 'placeholder': 'Your name'}, validators=[validators.DataRequired(message='Full name is required.')])
     email = StringField('Email', render_kw={'class': 'form-control', 'placeholder': 'Your email'}, validators=[validators.DataRequired(message='Email is required.'), validators.Email()])
@@ -8,3 +10,7 @@ class LandingForm(FlaskForm):
     def validate_email(self, field):
         if field.data.endswith('.edu'):
             raise ValidationError('Do not use a school email address.')
+        obj = EmailSignup.query.filter_by(email=field.data).first()
+        if obj is not None:
+            msg = 'This email was already added.'
+            raise ValidationError(msg)
--- landing/home/views.py
+++ landing/home/views.py
@@ -2,12 +2,18 @@ from flask import render_template, request
 from landing import app
 
 from .forms import LandingForm
+from .models import EmailSignup
 
 @app.route('/', methods=['GET','POST'])
 def home():
     form = LandingForm()
     if form.validate_on_submit():
-        print(form.full_name.data)
-        print(form.email.data)
-       
+        # print(form.full_name.data)
+        # print(form.email.data)
+        data = form.data
+        if 'csrf_token' in data:
+            del data['csrf_token']
+        obj = EmailSignup(**data)
+        obj.save()
+
     return render_template('home.html', form=form)
