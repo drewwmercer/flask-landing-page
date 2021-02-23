9ea30e181351c418c22fee769d10ba4674f57caf
drewwmercer <awmercer@gmail.com>
2021-02-22 20:18:48 -0500

    custom validation is working

--- landing/home/forms.py
+++ landing/home/forms.py
@@ -1,6 +1,10 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, validators
+from wtforms import StringField, validators, ValidationError
 
 class LandingForm(FlaskForm):
-    full_name = StringField('Full Name', validators=[validators.DataRequired(message='Full name is required')])
-    email = StringField('Email', validators=[validators.DataRequired(message='Email is required')])
+    full_name = StringField('Full Name', validators=[validators.DataRequired(message='Full name is required.')])
+    email = StringField('Email', validators=[validators.DataRequired(message='Email is required.'), validators.Email()])
+
+    def validate_email(self, field):
+        if field.data.endswith('.edu'):
+            raise ValidationError('Do not use a school email address.')
